d222a421f57df19084aad3be6ddcc436783817cd
Teodora Shumanova <t.shumanova@abv.bg>
2022-01-24 14:58:34 +0200

    tasks

---
+++ For Loop - Lab/src/P04_Number4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class P04_Number4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = Integer.parseInt(scanner.nextLine());
+        for (double step = 0; step <=n ; step+=2) {
+            System.out.printf("%.0f\n",Math.pow(2,step));
+        }
+    }
+}
