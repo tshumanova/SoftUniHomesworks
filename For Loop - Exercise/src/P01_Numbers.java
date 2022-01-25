9b938695a6dcbdbfdfb532b93303c3c6f480f609
Teodora Shumanova <t.shumanova@abv.bg>
2022-01-25 15:33:26 +0200

    task+exercises

---
+++ For Loop - Exercise/src/P01_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class P01_Numbers {
+    public static void main(String[] args) {
+
+        for (int i = 7; i <= 997; i++) {
+            if (i % 10 == 7) {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
